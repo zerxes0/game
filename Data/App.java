01b0a3da1859ae569b7791cc567098216bee7603
arturo <arturo7xz@gmail.com>
2017-06-11 14:38:34 -0600

    Reworked

--- Data/App.java
+++ Data/App.java
@@ -1,15 +1,15 @@
 package Data;
 
-import javax.swing.JFrame;
-
-import GUI.GameFrame;
+import java.awt.Dimension;
 
 public class App {
 
     public static void main( String[] args ){
-        GameFrame window = new GameFrame();
+    	Game window = new Game( new Dimension(800,600) );
+    	window.init();
+        //GameFrame window = new GameFrame();
         //evitamos un warning
-        window.init(800,600);
-        window.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );        
+        //window.init(800,600);
+        //window.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );        
     }
 }
