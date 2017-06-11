1ec766d018529e6d301fe40061c77151f36cb3ec
arturo <arturo7xz@gmail.com>
2017-06-10 18:47:03 -0600

    Created and most of the work done

--- maps/GameMap.java
+++ maps/GameMap.java
@@ -1,5 +1,24 @@
 package maps;
 
-public class GameMap {
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
+import entity.GameObject;
+import systems.SpriteSheet;
+
+public class GameMap extends GameObject {
+
+    private SpriteSheet sheet;
+    private ArrayList<BufferedImage> tile;
+
+    public GameMap( SpriteSheet sheet, int widht, int height ){
+        super( widht, height );
+        this.sheet = sheet;
+    }
+
+    public void setSheet(SpriteSheet tiles) { this.sheet = tiles; }
+    public void setTiles(ArrayList<BufferedImage> tile) { this.tile = tile; }
+
+    public SpriteSheet getSheet() { return sheet; }
+    public ArrayList<BufferedImage> getTiles() { return tile; }
 }
