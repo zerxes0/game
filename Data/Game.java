781849677f802420b9436f5b718ccb912f506595
arturo <arturo7xz@gmail.com>
2017-06-17 13:27:35 -0600

    Adapted to ticker changes

--- Data/Game.java
+++ Data/Game.java
@@ -7,12 +7,11 @@ import java.awt.image.BufferStrategy;
 
 import GUI.GameFrame;
 //import Data.Player;
+import systems.Time;
 import StateMachine.GameStateManager;
 
 public class Game{
 	//ticker
-    private int fps = 12; //frames per second
-	private long targetTime = 1000 / fps; //milisegundos entre fps
 	private boolean running = true;
 	//-----------------------------
 	
@@ -27,9 +26,10 @@ public class Game{
     	gameStateMachine = new GameStateManager( dimension );
     	KeyListener[] l = gameStateMachine.getComponent().getKeyListeners();
     	window.getCanvas().addKeyListener( l[0]);
+    	window.getCanvas().requestFocus();
     }
 
-    public void update(){    	
+    private void update(){    	
         gameStateMachine.draw();
     }
     
@@ -41,39 +41,26 @@ public class Game{
     	catch(Exception e){
     		e.printStackTrace();
     	}
-    	Ticker();  	
+    	Time.start();    	
         gameStateMachine.world();
+        start();
+    }
+    
+    private void start(){    	
+		while( running ){
+			try{			
+				g = bs.getDrawGraphics();
+	    		gameStateMachine.setG(g);
+				update();
+				bs.show();
+				g.dispose();
+				Thread.sleep( Time.getTime() );
+			}catch( Exception e ){
+				e.getStackTrace();
+			}
+		}		
     }
     
     public static Graphics getG(){ return g; } 
-
-    private void Ticker(){
-    	Thread thread = new Thread() {
-			public void run(){
-                //ticker -- se usa para manejar el tiempo del juego				
-				long start;
-				long elapsed;
-				long wait;
-
-				while( running ){
-					start = System.nanoTime();//obtenemos tiempo inicial
-					elapsed = System.nanoTime() - start; //final - inicial == tiempo transcurrido
-					wait = targetTime - elapsed / 1000000;
-					try{			
-						g = bs.getDrawGraphics();
-						gameStateMachine.setG(g);
-						update();
-						Thread.sleep(wait);
-						bs.show();
-						g.dispose();
-
-					}catch( Exception e ){
-						e.getStackTrace();
-					}
-				}		
-			}
-		};
-		thread.start();
-    }//ticker
-
+    
   }
