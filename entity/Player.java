0fb47c3c1d3bc607102a3dfde285ef586a74413f
arturo <arturo7xz@gmail.com>
2017-06-21 22:17:40 -0600

    unchanged

--- entity/Player.java
+++ entity/Player.java
@@ -42,18 +42,22 @@ public class Player extends Character {
             case "up":
                  pos.y -= velocity;
                  origin.y -= velocity;
+                 updateBounds();
                 break;
             case "left":
                  pos.x -= velocity;
                  origin.x -= velocity;
+                 updateBounds();
                 break;
             case "right":
                  pos.x += velocity;
                  origin.x += velocity;
+                 updateBounds();
                 break;
             case "down":
                  pos.y += velocity;
                  origin.y += velocity;
+                 updateBounds();
                 break;
             default:
                 break;
@@ -63,20 +67,25 @@ public class Player extends Character {
     public boolean checkCollision( int colX, int colY ){
             int x = (int) ( collider.getBounds().getX() );
             int y = (int) ( collider.getBounds().getY() );
-            int py0 = (int) (colY*1.5), px0 = colX;
-            int py1 = colY, px1 = (int) (colX*1.5);
-            int py2 = colY+16, px2 = (int) (colX*1.5);
-            int py3 = (int) (colY*1.5), px3 = colX+64;
+            int py0 = colY+16, px0 = colX;
+            int py1 = colY, px1 = colX +32 ;
+            int py2 = colY+32, px2 = colX + 32;
+            int py3 = colY+16, px3 = colX+64;          
+            
             x /= 64; y /= 16;
             x *= 64; y *= 16;
             System.out.println( "j( " + (x) + ", " + (y) + " )" );
             System.out.println();
 
             //PIXEL PERFECT COLLITIONS
-            boolean colFromBottom = ( x == px0 || y == py0 ) || ( x == px2 || y == py2 ) || ( x == px3 || y == py3 ) ;
-            boolean colFromLeft = ( x == px0 || y == py0 ) || ( x == px1 || y == py1 ) || ( x == px2 || y == py2 ) ;
-            boolean colFromRight = ( x == px2 || y == py2 ) || ( x == px1 || y == py1 ) || ( x == px2 || y == py2 ) ;
-            boolean colFromUp = ( x == px0 || y == py0 ) || ( x == px1 || y == py1 ) || ( x == px3 || y == py3 ) ;
+            boolean p0 = (x == px0 || y == py0) || (x*2 == px0 || y == py0) || (x == px0 || y*2 == py0) || (x*2 == px0 || y*2 == py0);
+            boolean p1 = (x == px1 || y == py1) || (x*2 == px1 || y == py1) || (x == px1 || y*2 == py1) || (x*2 == px1 || y*2 == py1);
+            boolean p2 = (x == px2 || y == py2) || (x*2 == px2 || y == py2) || (x == px2 || y*2 == py2) || (x*2 == px2 || y*2 == py2);
+            boolean p3 = (x == px3 || y == py3) || (x*2 == px3 || y == py3) || (x == px3 || y*2 == py3) || (x*2 == px3 || y*2 == py3);
+            boolean colFromBottom =  (p0 || p2 || p3);
+            boolean colFromLeft = (p0 || p1 || p2);
+            boolean colFromRight = (p2 || p1 || p2);
+            boolean colFromUp = (p0 || p1 || p3);
 
             if( colFromBottom )
                 return true;
